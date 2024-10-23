2424449e1d1f1908a10ed01f37a1b1afc82dab60
Kuh.rmdhn <113331985+kuhrmdhn@users.noreply.github.com>
2024-10-23 19:58:01 +0700

    hide scrollbar

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Logo from '../element/Logo'
+
+export default function Footer() {
+  return (
+    <footer className='h-20 w-full flex justify-center items-center bg-foreground'>
+      <Logo/>
+    </footer>
+  )
+}
